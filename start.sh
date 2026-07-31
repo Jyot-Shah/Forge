7058ec688923bba3f78b8917312e67b436a17898
Jyot Shah <jyotshah1595@gmail.com>
2026-07-31 20:11:52 +0530

    fix(dns): enforce ipv4first at the OS level to bypass undici Node 20 socket failures

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export NODE_OPTIONS="--dns-result-order=ipv4first"
 # Start the background worker in a detached thread
 npm run start -w apps/worker &
 # Start the primary web API in the main thread
